74fd3b56463e6a46c824ecfebb72021e7ae73a41
maicol783 <maicolocampoareiza783@gmail.com>
2021-09-21 17:35:45 -0500

    conexion mongodb

--- foto-servicio/src/main/java/pragma/practica/back/foto/entidades/Foto.java
+++ foto-servicio/src/main/java/pragma/practica/back/foto/entidades/Foto.java
@@ -1,15 +1,18 @@
 package pragma.practica.back.foto.entidades;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@AllArgsConstructor @NoArgsConstructor
+@Document
+
+@AllArgsConstructor @NoArgsConstructor @Builder
 public class Foto {
-
-
+    @Id
     private int idFoto;
     private String nombre;
 
