e9f99d674d6de59715f2c913bc4e7f42079e608e
maicol783 <maicolocampoareiza783@gmail.com>
2021-09-21 17:03:17 -0500

    conexion mongodb

---
+++ foto-servicio/src/main/java/pragma/practica/back/foto/FotoServicioApplication.java
@@ -0,0 +1,13 @@
+package pragma.practica.back.foto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotoServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotoServicioApplication.class, args);
+	}
+
+}
---
+++ foto-servicio/src/test/java/pragma/practica/back/foto/FotoServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package pragma.practica.back.foto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotoServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
